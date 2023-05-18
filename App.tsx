9635f75e253edefb0ebf847911f9564152dc9a06
Antonio Carlos <antonie.dev@gmail.com>
2023-05-18 15:16:31 -0300

    add-home-page

---
+++ App.tsx
@@ -0,0 +1,44 @@
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree';
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
+import { StatusBar } from "expo-status-bar";
+import { styled } from "nativewind";
+import { ImageBackground, Text, TouchableOpacity, View } from "react-native";
+
+import NlwLogo from './src/assets/logo.svg';
+import blurBg from './src/assets/luz.png';
+import Strips from './src/assets/stripes.svg';
+
+const StyledStripes  = styled(Strips)
+
+export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular, Roboto_700Bold,BaiJamjuree_700Bold
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+  return (
+    <ImageBackground source={blurBg} className="bg-gray-900 px-8 py-10 items-center flex-1 relative" imageStyle={{position: 'absolute', left: '-100%'}}>
+     <StyledStripes className=" absolute left-2" /> 
+
+     <View className="flex-1 items-center justify-center gap-6">
+      <NlwLogo />
+
+      <View className='space-y-2'>
+        <Text className="text-center font-title text-2xl leading-tight text-gray-50">Sua cápsula do tempo</Text>
+        <Text className="text-center font-body text-base leading-relaxed text-gray-100">Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!</Text>
+      </View>
+
+      <TouchableOpacity activeOpacity={0.5} className="rounded-full bg-green-500 px-5 py-2">
+        <Text className="font-alt text-sm uppercase text-black">COMEÇAR A CADASTRAR</Text>
+      </TouchableOpacity>
+     </View>
+
+     <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+      Feito com 💜 por Antonio Carlos 
+     </Text>
+      <StatusBar style="light" translucent />
+    </ImageBackground >
+  );
+}
